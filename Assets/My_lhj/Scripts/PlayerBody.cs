f9de2b82c737fcd91ff5ed7899566a0d940e1934
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2024-05-14 06:54:23 +0900

    Player bloodyScreen
    플레이어가 피해를 입을 경우 화면에 효과 발생

--- Assets/My_lhj/Scripts/PlayerBody.cs
+++ Assets/My_lhj/Scripts/PlayerBody.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBody : MonoBehaviour
 {
     public int HP = 100;
+    public GameObject bloodyScreen;
 
     public void TakeDamage(int damageAmount)
     {
@@ -13,12 +15,54 @@ public class PlayerBody : MonoBehaviour
         if(HP <= 0)
         {
             print("Player Dead");
+            //플레이어 dead 코드
         }
 
         else
         {
             print("Player Hit");
+            StartCoroutine(BloodyScreenEffect());
         }
+    }
+
+    private IEnumerator BloodyScreenEffect()
+    {
+        if (bloodyScreen.activeInHierarchy == false)
+        {
+            bloodyScreen.SetActive(true);
+        }
+
+        var image = bloodyScreen.GetComponentInChildren<Image>();
+
+        // Set the initial alpha value to 1 (fully visible).
+        Color startColor = image.color;
+        startColor.a = 1f;
+        image.color = startColor;
+
+        float duration = 3f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            // Calculate the new alpha value using Lerp.
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+
+            // Update the color with the new alpha value.
+            Color newColor = image.color;
+            newColor.a = alpha;
+            image.color = newColor;
+
+            // Increment the elapsed time.
+            elapsedTime += Time.deltaTime;
+
+            yield return null; ; // Wait for the next frame.
+        }
+
+        if (bloodyScreen.activeInHierarchy)
+        {
+            bloodyScreen.SetActive(false);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
