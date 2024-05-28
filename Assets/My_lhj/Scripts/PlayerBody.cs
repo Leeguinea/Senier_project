196d8b2c036518c87ef6791777bd15c2074f535a
Leeguinea <129941496+Leeguinea@users.noreply.github.com>
2024-05-28 20:01:48 +0900

    fpsPlayer
    arms_handgun_01에 스크립트 추가 (데미지, 헬스바)

--- Assets/My_lhj/Scripts/PlayerBody.cs
+++ Assets/My_lhj/Scripts/PlayerBody.cs
@@ -32,7 +32,6 @@ public class PlayerBody : MonoBehaviour
         else
         {
             print("Player Hit");
-            StartCoroutine(BloodyScreenEffect());
         }
     }
 
@@ -47,46 +46,6 @@ public class PlayerBody : MonoBehaviour
         SceneManager.LoadScene("GameOverScene"); // 게임 오버 씬 로드
     }
 
-
-    private IEnumerator BloodyScreenEffect()
-    {
-        if (bloodyScreen.activeInHierarchy == false)
-        {
-            bloodyScreen.SetActive(true);
-        }
-
-        var image = bloodyScreen.GetComponentInChildren<Image>();
-
-        // Set the initial alpha value to 1 (fully visible).
-        Color startColor = image.color;
-        startColor.a = 2f;
-        image.color = startColor;
-
-        float duration = 3f;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            // Calculate the new alpha value using Lerp.
-            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
-
-            // Update the color with the new alpha value.
-            Color newColor = image.color;
-            newColor.a = alpha;
-            image.color = newColor;
-
-            // Increment the elapsed time.
-            elapsedTime += Time.deltaTime;
-
-            yield return null; ; // Wait for the next frame.
-        }
-
-        if (bloodyScreen.activeInHierarchy)
-        {
-            bloodyScreen.SetActive(false);
-        }
-
-    }
 
     private void OnTriggerEnter(Collider other)
     {
